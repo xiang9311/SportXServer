90181cea16a7b856afe6668ed6ffcb100899a7ca
xiang <xiang9311@sina.cn>
2016-05-25 10:58:03 +0800

    协议修改

--- Background/views.py
+++ Background/views.py
@@ -94,8 +94,8 @@ def getRongToken(request):
         userService.userExist(request_common.userid)
     except:
         #未注册用户
-        initCommonErrorResponse(cmdId, 101, response__pro.common)
-        return HttpResponse(response__pro.SerializeToString())
+        initCommonErrorResponse(cmdId, 101, response_pro.common)
+        return HttpResponse(response_pro.SerializeToString())
 
 
     #response
@@ -321,6 +321,43 @@ def guanzhuUser(request):
     except Exception as error:
         log.error(str(error))
         initCommonErrorResponse(cmdId, 101, response_pro.common)
+        return HttpResponse(response_pro.SerializeToString())
+
+@csrf_exempt
+def getMyCommentMessage(request):
+    cmdId = 10006
+    request_pro = pilot_pb2.Request10006()
+    response_pro = pilot_pb2.Response10006()
+    try:
+        request_pro.MergeFromString(request.read())
+    except Exception as error:
+        #如果读取异常直接返回一个error
+        log.error('comunications failed')
+        log.error(str(error))
+        initCommonErrorResponse(cmdId, 101, response_pro.common)
+        return HttpResponse(response_pro.SerializeToString())
+
+    request_common = request_pro.common
+    request_params = request_pro.params
+
+    log.info("搜索:数据解析成功" + str(request_params))
+
+    #构造返回
+
+    try:
+        response_common = response_pro.common
+        response_data = response_pro.data
+        initCommonResponse(0, 'success', cmdId, 0, response_common)
+        if request_params.userId:
+            if  userService.getMyCommentMessage(request_params.pageIndex,response_data):
+                log.info(cmdId+request_params.userId+"成功")
+                return HttpResponse(response_pro.SerializeToString())
+        else :
+            initCommonErrorResponse(cmdId, 1, response_common)
+            return HttpResponse(response_pro.SerializeToString())
+    except Exception as error:
+        log.error(str(error))
+        initCommonErrorResponse(cmdId, 103, response_pro.common)
         return HttpResponse(response_pro.SerializeToString())
 
 
