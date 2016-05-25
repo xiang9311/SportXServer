61a0f3ed0fdd8ccc35c649067004dd0c47217078
lzp <lzppp1114@gmail.com>
2016-05-25 21:03:11 +0800

    评论自己不推送，且目标是自己

--- Background/service/trendService.py
+++ Background/service/trendService.py
@@ -182,6 +182,8 @@ def createComment(trendId,createUser,toComment,toUser,content,gymId):
     tblTrendComment.createUser = tblCreateUser
     if toUser:
         tblTrendComment.toUserId = toUser
+    else:
+        tblTrendComment.toUserId = createUser
     if toComment:
         tblTrendComment.toCommentId = toComment
     if gymId:
@@ -204,7 +206,8 @@ def createComment(trendId,createUser,toComment,toUser,content,gymId):
         tblTrendComment.save()
         tblTrendCommentMassage.save()
         addTrendCommentCount(tblTrendComment.trend)
-        pushToUser(toUser,tblTrendComment.createUser.userName,tblTrend.id)
+        if toUser:
+            pushToUser(toUser,tblTrendComment.createUser.userName,tblTrend.id)
     except Exception as e:
         log.error(str(e))
         return False
