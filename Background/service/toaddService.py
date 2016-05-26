697c0a933875cb5c58c06dfb41c856aa062a3d95
lzp <lzppp1114@gmail.com>
2016-05-26 15:14:01 +0800

    10017in toadd

--- Background/service/toaddService.py
+++ Background/service/toaddService.py
@@ -1,3 +1,14 @@
 from Background.models import TblBriefUser,TblTrend ,TblLikeTrend ,TblCommentMessage
 from SportXServer import qiniuUtil, timeUtil ,log
-#10005
+#10017
+def getTrendBriefMessage(userId ,  responseData):
+    try:
+        hadRead = TblBriefUser.objects.get(id = userId).hadReadMessage
+        comments = TblCommentMessage.objects.filter(toUserId=userId , id__gt=hadRead).order_by('-id')
+    except Exception as e:
+        log.error(str(e))
+        return False
+    briefMessage = responseData.trendBriefMessage
+    briefMessage.lastAvatar = TblBriefUser.objects.get(id = comments[0].createUser.id).userAvatar
+    briefMessage.count = comments.count()
+    return True
